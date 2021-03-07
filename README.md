bf7d0b25543825eb83e67c48d747fe4ed6e543f9
slonka <slonka@users.noreply.github.com>
2021-03-08 00:05:03 +0100

    Make runnable from cmd

---
+++ README.md
@@ -0,0 +1,9 @@
+# cron-expression-parser
+
+## Running
+
+Example: 
+
+```bash
+./gradlew run --args='"*/15 0 5,10,15 * 1-5 /usr/bin/find"'
+```
--- src/main/kotlin/pl/slonka/cep/main.kt
+++ src/main/kotlin/pl/slonka/cep/main.kt
@@ -3,21 +3,26 @@ package pl.slonka.cep
 import pl.slonka.cep.converter.ExpressionToTable
 import kotlin.system.exitProcess
 
-fun main(args: Array<String>) {
-    if (args.size != 1) {
-        help()
-        exitProcess(1)
-    }
+class Main {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            if (args.size != 1) {
+                help()
+                exitProcess(1)
+            }
 
-    val input = args[0].removeSurrounding("\"")
-    try {
-        println(ExpressionToTable.convert(input))
-    } catch (e: Exception) {
-        println("Could not parse $input. Reason: $e")
-        exitProcess(2)
-    }
-}
+            val input = args[0].removeSurrounding("\"")
+            try {
+                println(ExpressionToTable.convert(input))
+            } catch (e: Exception) {
+                println("Could not parse $input. Reason: $e")
+                exitProcess(2)
+            }
+        }
 
-fun help() {
-    println("Please provide CRON expression as a command-line argument")
+        fun help() {
+            println("Please provide CRON expression as a command-line argument")
+        }
+    }
 }
